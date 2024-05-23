6c9d399de00a81797fea616752024d79cc730a0d
Chris Clothier <c.r.clothier@gmail.com>
2024-05-23 16:32:40 +0100

    Clone project from Rosemore Design

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const autoRotate = true;
+const intervalTime = 5000;
+let slideInterval;
+
+ const nextSlide = () => {
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, intervalTime);
+  const visible = document.querySelector('.visible');
+  visible.classList.remove('visible');
+  if (visible.nextElementSibling) {
+    visible.nextElementSibling.classList.add('visible');
+  } else {
+    slides[0].classList.add('visible');
+  }
+ }
+
+ const prevSlide = () => {
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, intervalTime);
+  const visible = document.querySelector('.visible');
+  visible.classList.remove('visible');
+  if (visible.previousElementSibling) {
+    visible.previousElementSibling.classList.add('visible');
+  } else {
+    slides[slides.length - 1].classList.add('visible');
+  }
+ }
+
+next.addEventListener('click', nextSlide);
+
+prev.addEventListener('click', prevSlide);
+
+if (autoRotate) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
+
+const header = document.querySelector('#header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 600) {
+    header.classList.add('header-scroll');
+  } else {
+    header.classList.remove('header-scroll');
+  }
+})
+
+console.log(window.screenY)
